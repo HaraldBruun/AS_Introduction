ec2414c2e55af27b751ea44a1ffe2e8d0739795d
antonoeschmidt <s163053@student.dtu.dk>
2019-09-16 10:28:23 +0200

    Hej

--- app/src/main/java/com/example/gruppe07_appsjov/Anton.java
+++ app/src/main/java/com/example/gruppe07_appsjov/Anton.java
@@ -7,19 +7,24 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Anton extends AppCompatActivity {
+public class Anton extends AppCompatActivity implements View.OnClickListener {
     Button enKnap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.anton);
         enKnap = findViewById(R.id.testButton);
-
+        enKnap.setOnClickListener(this);
     }
 
     public void onClick(View v) {
-        Toast toast = new Toast("Hej");
-
+        System.out.println("Hej");
+        Toast.makeText(this, "This is my Toast message!",
+                Toast.LENGTH_LONG).show();
     }
 
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
