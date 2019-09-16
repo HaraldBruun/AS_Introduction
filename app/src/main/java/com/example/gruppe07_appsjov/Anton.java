bf8e53707e25973386f2321b47ac6f3bce133a0b
antonoeschmidt <s163053@student.dtu.dk>
2019-09-16 10:10:37 +0200

    Hej

--- app/src/main/java/com/example/gruppe07_appsjov/Anton.java
+++ app/src/main/java/com/example/gruppe07_appsjov/Anton.java
@@ -1,17 +1,24 @@
 package com.example.gruppe07_appsjov;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Anton extends AppCompatActivity {
-    Button testKnap;
+    Button enKnap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        testKnap = new Button(this);
-        setContentView(testKnap);
+        setContentView(R.layout.anton);
+        enKnap = findViewById(R.id.testButton);
+
+    }
+
+    public void onClick(View v) {
+        Toast toast = new Toast("Hej");
 
     }
 
