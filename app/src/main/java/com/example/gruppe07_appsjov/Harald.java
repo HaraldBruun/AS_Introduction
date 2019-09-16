913e4a3f5ea33fe4032769b16403d78a3661069f
alexAbela <s175124@student.dtu.dk>
2019-09-16 11:29:09 +0200

    hello wankas

--- app/src/main/java/com/example/gruppe07_appsjov/Harald.java
+++ app/src/main/java/com/example/gruppe07_appsjov/Harald.java
@@ -1,18 +1,29 @@
 package com.example.gruppe07_appsjov;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Harald extends AppCompatActivity {
+public class Harald extends AppCompatActivity implements View.OnClickListener {
+    Button knap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Button knap;
+
         setContentView(R.layout.harald);
         knap = findViewById(R.id.button3);
         knap.setText("Hej smukke");
+        knap.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this,Main2Activity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/gruppe07_appsjov/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.example.gruppe07_appsjov.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
